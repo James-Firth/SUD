c458ecdbeaa89c9bde863bd6cb4e45cb87dd95bb
Kelaos <Kelaos@no-site.com>
2011-10-11 20:51:02 -0500

    Added Getters to Creature class, working on updating Player.java

--- src/sud/Mob.java
+++ src/sud/Mob.java
@@ -4,22 +4,5 @@ package sud;
  */
 class Mob extends Creature
 {
-    /*
-     * 
-     * Mob Constructor
-     * @param h Max Health
-     * @param a Attack
-     * @param d Defense
-     * @param Speed
-     */
-	public Mob(int h, int a, int d, int s)
-	{
-		currentHealth = h;
-		maxHealth = h;
-		attack = a;
-		defense = d;
-		speed = s;
-		level = 1;
-	}//end constructor
         	
 }
