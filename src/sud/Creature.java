448415827e630d92f15a4b31e55c49d6712e7470
Kelan Torch <torch@ilnaeas.ca>
2011-10-12 14:01:44 -0500

    I added a comment just to see if it would work

--- src/sud/Creature.java
+++ src/sud/Creature.java
@@ -1,7 +1,9 @@
 package sud;
 abstract class Creature
 {
-        protected String name;
+
+	//test comment to see if I can commit this to the files			
+	protected String name;
 	protected int maxHealth;
 	protected int currentHealth;
 	protected int attack;
@@ -10,74 +12,74 @@ abstract class Creature
 	protected int experience;
 	protected int level;
 
-        protected Creature(String name, int mHP, int cHP, int att, int def, int spd, int exp, int lvl)
-        {
-            this.name = name;
-            maxHealth = mHP;
-            currentHealth = cHP;
-            attack = att;
-            defense = def;
-            speed = spd;
-            experience = exp;
-            level = lvl;
-        }
-        
-        
-        //******************** GETTERS *****************
-        public String getName()
-        {
-            return name;
-        }
-        
-        public int getHealth()
-        {
-            return currentHealth;
-        }
-        
+		protected Creature(String name, int mHP, int cHP, int att, int def, int spd, int exp, int lvl)
+		{
+			this.name = name;
+			maxHealth = mHP;
+			currentHealth = cHP;
+			attack = att;
+			defense = def;
+			speed = spd;
+			experience = exp;
+			level = lvl;
+		}
+		
+		
+		//******************** GETTERS *****************
+		public String getName()
+		{
+			return name;
+		}
+		
+		public int getHealth()
+		{
+			return currentHealth;
+		}
+		
 	public int getMaxHealth()
 	{
 		return maxHealth;
 	}
-        
-        public int getAttack()
-        {
-            return attack;
-        }
-        
-        public int getDefense()
-        {
-            return defense;
-        }
-        
-        public int getSpeed()
-        {
-            return speed;
-        }
-        
-        public int getExp()
-        {
-            return experience;
-        }
-        
-        public int getLevel()
-        {
-            return level;
-        }
-        
+		
+		public int getAttack()
+		{
+			return attack;
+		}
+		
+		public int getDefense()
+		{
+			return defense;
+		}
+		
+		public int getSpeed()
+		{
+			return speed;
+		}
+		
+		public int getExp()
+		{
+			return experience;
+		}
+		
+		public int getLevel()
+		{
+			return level;
+		}
+		
 	public String toString()
 	{
 		return "Health: "+ currentHealth + "\nAttack: "+  attack + "\nDefense: " + defense + "\nSpeed: " + speed +"\n";
 	}
-        
-        
-        //**************  SETTERS ******************
+		
+		
+		//**************  SETTERS ******************
 	protected void takeDamage(int taken)
 	{
 		currentHealth = currentHealth - taken;
 	}
-        
-        protected void giveHealth(int given)
-        {
-               currentHealth = currentHealth - given; 
-        }
+		
+		protected void giveHealth(int given)
+		{
+			   currentHealth = currentHealth - given; 
+		}
 }
