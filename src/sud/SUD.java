651db35a995e43d857169c4f133e99bfdb97e874
Kelan Torch <torch@ilnaeas.ca>
2011-10-12 15:54:53 -0500

    Working on the MENU

--- src/sud/SUD.java
+++ src/sud/SUD.java
@@ -1,18 +1,59 @@
-package sud;
+//package sud;
 
-/**
- *
- * @author James
- */
-public class SUD {
+import java.util.Scanner;
 
-    /**
-     * @param args no arguments
-     * 
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-        Mob monster = new Mob(1,2,3,4);
-        //EXAMPLES!
-    }
+public class SUD 
+{
+
+	public static void main(String[] args) 
+	{
+		System.out.println("Test");
+
+		menu();
+		//Mob monster = new Mob(1,2,3,4);
+	}
+	public static void menu()
+	{
+		String output = "What would you like to do?";
+		String response = "";
+		boolean keepGoing = true;
+
+		while (keepGoing)
+		{
+			print(output);
+			response = getValidString(output);
+			if(response.equalsIgnoreCase("help"))
+			{
+				String help = "This is the help section";
+				print(output);
+			}
+			else if (response.equalsIgnoreCase("map" ) || response.equalsIgnoreCase("draw"))
+			{
+				//draw map method or class here
+				print("DRAW MAP HERE");
+			}
+			else if (response.equalsIgnoreCase("check"))
+			{
+				//check what?
+				response = getValidString("Check What?");
+				print("CHECK");
+			}
+
+		}
+	}
+	public static String getValidString(String output)
+	{
+		//get input and trim the output
+		String test = "";
+		Scanner scan = new Scanner(System.in);
+		System.out.println(output);
+		test = scan.next();
+		test = test.trim();
+
+		return test;
+	}//end getValidString
+	public static void print(String output)
+	{
+		System.out.println(output);
+	}//end print
 }
