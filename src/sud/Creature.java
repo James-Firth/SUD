d38c4ea2d9c11ca1c0f26f323772029f0605d80c
Kelan Torch <torch@ilnaeas.ca>
2011-10-12 14:16:50 -0500

    Updated Authors

--- src/sud/Creature.java
+++ src/sud/Creature.java
@@ -1,10 +1,6 @@
 package sud;
 abstract class Creature
-{
-
-	//test comment to see if I can commit this to the files	
-	//Third Comment
-	//Second Comment		
+{	
 	protected String name;
 	protected int maxHealth;
 	protected int currentHealth;
