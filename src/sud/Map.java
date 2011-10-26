7f6b10eb7a1fd6649b3d1c43b1501e862c325dde
Kelan Torch <torch@ilnaeas.ca>
2011-10-26 16:19:34 -0500

    Added Stuffs to the map and room classes, it's not working yet.... ideas?

--- src/sud/Map.java
+++ src/sud/Map.java
@@ -1,4 +1,10 @@
+import java.awt.Point;
+
 class Map
 {
-	
+	Room [][][ test = new Room[2][2];
+	Point location = new Point(0,0);
+
+	test[0][0] = new Room(false, false, location);
+
 }
--- src/sud/Room.java
+++ src/sud/Room.java
@@ -1,12 +1,17 @@
-class Room
+import java.awt.Point;
+
+class Room 
 {
 	protected boolean inRoom;
 	protected boolean hasVisit;
+	protected Point location;
 
-	public Room(boolean inRoom, boolean hasVisit)
+	public Room(boolean inRoom, boolean hasVisit, Point location)
 	{
 		this.inRoom = inRoom;
 		this.hasVisit = hasVisit;
+		this.location = location;
+
 	}//end constructor
 
 	public String toString()
